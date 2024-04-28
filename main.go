42d749796fb36e3dfe23a4b0b169d6b5bd0cf311
Duarte <duartydev@gmail.com>
2024-04-28 17:36:27 -0400

    feat: add create product fuction

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -11,4 +13,37 @@ func main() {
 	}
 	defer db.Close()
 
+	product := CreateProduct("SpaceShip", 10.02)
+	err = InsertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func CreateProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price,
+	}
+}
+
+func InsertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("insert into products(id, name, price) values(?, ?, ?)")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		panic(err)
+	}
+	return nil
 }
