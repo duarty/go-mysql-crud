bc69d3921ba1a7431c4b0510bb0c56b20ae81297
José Duarte <duartydev@gmail.com>
2025-07-20 21:56:49 -0400

    Delete front-end directory

--- front-end/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    "@babel/preset-env", // Includes support for modern JavaScript syntax
-    "@babel/preset-react", // Includes support for JSX syntax
-    "@babel/preset-typescript", // Includes support for TypeScript syntax
-  ],
-};
